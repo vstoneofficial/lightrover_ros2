ece50d905ed06315bf3f42b2132e4aa06c9a8077
baba.r <baba.r@vstone.co.jp>
2024-04-26 09:57:16 +0900

    #1 暫定対応

--- lightrover_ros/launch/nav_base.launch.py
+++ lightrover_ros/launch/nav_base.launch.py
@@ -5,7 +5,6 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
@@ -13,11 +12,11 @@ from launch.substitutions import LaunchConfiguration, Command
 from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_share_path
 from launch_ros.parameter_descriptions import ParameterValue
+from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
     description_package_path = get_package_share_path('lightrover_description')
     default_model_path = description_package_path / 'urdf/lightrover_urdf.xacro'
-    default_rviz_config_path = description_package_path / 'rviz/lightrover.rviz'
     
     launch_ydlidar = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
@@ -33,17 +32,31 @@ def generate_launch_description():
                                       description='Absolute path to robot urdf file')
     robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
                                        value_type=str)
+    
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': robot_description}]
     )
     
+    left_static_transform_publisher_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments = ['0', '0.072', '0.006', '0', '0', '0', 'base_link', 'left_wheel_link']
+    )
+    
+    right_static_transform_publisher_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments = ['0', '-0.072', '0.006', '3.141592', '0', '0', 'base_link', 'right_wheel_link']
+    )
 
     return LaunchDescription([  
         launch_ydlidar,
         model_arg,
         robot_state_publisher_node,
+        left_static_transform_publisher_node,
+        right_static_transform_publisher_node,
         Node(package='lightrover_ros', executable='i2c_controller', output='screen'),
         Node(package='lightrover_ros', executable='odom_manager', output='screen'),
         Node(package='lightrover_ros', executable='pos_controller', output='screen'),
