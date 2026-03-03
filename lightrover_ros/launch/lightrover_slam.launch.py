f8751e87b7bdc78e4223cb1bf453a8289b4aa35b
baba.r <baba.r@vstone.com>
2026-03-03 11:13:45 +0900

    SLAMのLiDARの設定ファイルの読込先を変更

--- lightrover_ros/launch/lightrover_slam.launch.py
+++ lightrover_ros/launch/lightrover_slam.launch.py
@@ -11,7 +11,7 @@ import lifecycle_msgs.msg
 import os
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('ydlidar_ros2_driver')
+    share_dir = get_package_share_directory('lightrover_ros')
     rviz_config_file = os.path.join(share_dir, 'config','ydlidar.rviz')
     parameter_file = LaunchConfiguration('params_file')
     node_name = 'ydlidar_ros2_driver_node'
